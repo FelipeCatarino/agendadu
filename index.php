96266116a1fae9c78fd35004d3765957e4617a67
Felipe da Costa Catarino <felipecatarino577@gmail.com>
2024-04-08 18:52:36 -0300

    primeira versão

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="x-UA-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="images/icons/icons8-manutenção-48.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Agendadu</title>
+</head>
+<body>
+
+    <header>
+        <div class="banner">
+            <img src="images/banner.png" alt="error load banner img ">
+        </div>
+        <ul class="menu">
+            <li><a href="super.html">login</a></li>
+        </ul>
+    </header>
+    
+
+    <main>
+        <h1>Preencha abaixo para agendar seu horário</h1>
+        <hr><br>  
+
+        <div class="atendent">
+
+            <form action="www.google.com" method="post">
+
+                <input class="text-box" type="text" placeholder="Nome"> <br>
+               
+                <input class="text-box" type="text" placeholder="Modelo moto"> <br>
+                
+                <input class="text-box" type="tel" telefone placeholder="Numero"> <br>
+
+                
+                <select class="text-box" name="service" id="" value="service">
+                    <option value="relação">relação</option>
+                    <option value="oleo">Oléo 20w50</option>
+                    <option value="pneu">Pneu</option>
+                    <option value="pastilha">pastilha</option>
+                </select> <br>
+                
+                <input class="text-box" type="date"> <br>
+
+                <div style="display: flex; justify-content: center; padding-top: 10px;">
+                <input class="buttons" type="submit">
+                </div>
+            </form>
+            <br><br>
+
+            
+        </div>
+
+    </main>
+
+
+    <footer>
+        <div class="rodape">
+            - amiguinhos do código ® - todos os direitos reservados
+        </div>
+    </footer>
+</body>
+</html>
---
+++ super.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="x-UA-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="images/icons/icons8-manutenção-48.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Agendadu</title>
+</head>
+<body>
+
+    <header>
+        <div class="banner">
+            <img src="images/banner.png" alt="error load banner img ">
+        </div>
+    </header>
+    
+
+    <main>
+        <div class="formulario">
+            <h1>Preencha abaixo para agendar seu horário</h1>
+            
+            <form class="atendent" action="www.google.com" method="post">
+                <hr><br>    Nome:
+                <input class="text-box" type="text"> <br>
+                Qual sua moto:
+                <input class="text-box" type="text"> <br>
+                Numero de telefone:
+                <input class="text-box" type="number"> <br>
+
+                serviço:
+                <select class="text-box" name="service" id="" value="service">
+                    <option value="relação">relação</option>
+                    <option value="oleo">Oléo 20w50</option>
+                    <option value="pneu">Pneu</option>
+                    <option value="pastilha">pastilha</option>
+                </select> <br>
+
+                Data para agendamento:
+                <input class="text-box" type="date"> <br>
+
+                <br><hr><br>
+
+                <input class="buttons" type="submit">
+
+            </form>
+        </div>
+
+    </main>
+
+
+    <footer>
+        <div class="rodape">
+            - amiguinhos do código ® - todos os direitos reservados
+        </div>
+    </footer>
+</body>
+</html>
